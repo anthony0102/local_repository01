97d9258ab849c06048e4d1ef0176b33ddfa559f4
anthony0102 <anthonylfam@163.com>
2022-08-31 16:38:56 +0800

    deleted left main.py

--- projectHtml/chapter9/period4-4/vue-demo/tabledata.php
+++
@@ -1,8 +0,0 @@
-<?php
-     header('Content-Type: application/json');
-      header('Content-Type: text/html;charset=utf-8');
-    header('Access-Control-Allow-Origin:*');
-    header('Access-Control-Allow-Methods:*');
-    header('Access-Control-Allow-Headers:x-requested-with,content-type');
-
-    echo file_get_contents('tabledata.json');
